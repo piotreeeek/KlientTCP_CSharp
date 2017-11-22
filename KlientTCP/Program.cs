d5797640b4d4fb5259891f6fdd64677dcfaeb933
Piotrek <piotrek120017@gmail.com>
2017-11-22 22:18:33 +0100

    file sending support

--- KlientTCP/Program.cs
+++ KlientTCP/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -63,12 +66,60 @@ namespace KlientTCP
                     }
                     if (networkStream.CanRead)
                     {
-                        byte[] bytes = new byte[(int)tcpClient.ReceiveBufferSize];
+                        if (sendData.Split(' ').First().ToUpper() == "GET")
+                        {
+                            byte[] bytes = new byte[1024];
 
-                        networkStream.Read(bytes, 0, (int)tcpClient.ReceiveBufferSize);
+                            int length = networkStream.Read(bytes, 0, bytes.Length);
 
-                        string returnData = Encoding.UTF8.GetString(bytes);
-                        Console.WriteLine("odebrano od serwera: " + returnData);
+                            string returnData = Encoding.UTF8.GetString(new List<byte>(bytes).GetRange(0, length).ToArray());
+                            if (returnData.ToUpper() == "NO_FILE")
+                            {
+                                Console.WriteLine("odebrano od serwera: Brak takiego pliku.");
+                            }
+                            else
+                            {
+                                string[] splitted = returnData.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                                Dictionary<string, string> headers = new Dictionary<string, string>();
+                                foreach (string str in splitted)
+                                {
+                                    if (str.Contains(":"))
+                                    {
+                                        headers.Add(str.Substring(0, str.IndexOf(":")), str.Substring(str.IndexOf(":") + 1));
+                                    }
+
+                                }
+                                //Get filesize from header
+                                int filesize = Convert.ToInt32(headers["Content-length"]);
+                                //Get filename from header
+                                string filename = headers["Filename"];
+                                int bufferCount = Convert.ToInt32(Math.Ceiling((double)filesize / (double)1024));
+                                FileStream fs = new FileStream("D:\\politechnika\\semestr_7\\sieciowe\\przykladowe\\klient\\" + filename, FileMode.OpenOrCreate);
+
+                                while (filesize > 0)
+                                {
+                                    byte[] buffer = new byte[1024];
+
+                                    int size = networkStream.Read(buffer, 0, 1024);
+
+                                    fs.Write(buffer, 0, size);
+
+                                    filesize -= size;
+                                }
+
+
+                                fs.Close();
+                            }
+                        }
+                        else
+                        {
+                            byte[] bytes = new byte[1024];
+
+                            int length = networkStream.Read(bytes, 0, bytes.Length);
+
+                            string returnData = Encoding.UTF8.GetString(new List<byte>(bytes).GetRange(0, length).ToArray());
+                            Console.WriteLine("odebrano od serwera: " + returnData);
+                        }
                     }
 
                     Console.WriteLine("Jeśli chcesz znowu coś nadac wciśnij klawisz Y. W przeciwnym wypadku wciśnij dowolny klawisz");
